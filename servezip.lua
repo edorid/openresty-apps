9146ad147cd41ce0415fbbfd7efb5dc3ce22a1d7
me <me@ed.or.id>
2014-09-12 15:53:14 +0000

    Fix file naming

--- servezip.lua
+++ servezip.lua
@@ -7,7 +7,7 @@
     zip_file => path to zip file
     zip_cachedir => directory of extracted files
     zip_path => path of file inside zip archive
-    zip_redirect => set to 'on' if you want to serve existing extracted file without passing to lua; useful for content-range
+    zip_redirect => set to 'on' if you want to serve existing extracted file without passing to lua
 
   Example usage:
     - Without redirect:
@@ -16,13 +16,13 @@
 		root /tmp/cache;
 		set $zip_cachedir /tmp/cache;
 		set $zip_file /tmp/assets.zip;
-		content_by_lua_file "apps/zip.lua";
+		content_by_lua_file "apps/servezip.lua";
 	}
 
     - With redirect:
 
 	location @zip_server {
-		content_by_lua_file "apps/zip.lua";
+		content_by_lua_file "apps/servezip.lua";
 	}
 
 	location ~ /assets/(?<zip_path>.*)$ {
@@ -34,9 +34,14 @@
 	}
 --]]
 
+--[[function getrange(rangehdr, filesize)
+  return 1
+end--]]
+
 local zip = require('zip')
 local path = require('pl.path')
 local dir = require('pl.dir')
+local stringx = require('pl.stringx')
 local zip_file, zip_cachedir, zip_path = ngx.var.zip_file, ngx.var.zip_cachedir, ngx.var.zip_path
 local redirect = ngx.var.zip_redirect == 'on'
 
@@ -44,9 +49,8 @@ local _, _, uri2dir = string.find(ngx.var.uri, '/(.+)/' .. zip_path)
 local dest_dir = path.join(zip_cachedir, uri2dir, path.dirname(zip_path))
 local dest_file = path.join(dest_dir, path.basename(zip_path))
 
-local fx
-local data
--- check existing file on cache
+local fx, data, ofstart, ofend
+-- check existing file on cache; with redirect on, this should never executed
 if path.isfile(dest_file) then
   fx = io.open(dest_file,"r")
   repeat
